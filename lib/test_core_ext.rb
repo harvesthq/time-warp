dd044225985b0f224f010cfba9b42a98eb4f8273
Barry Hess <barry@bjhess.com>
2008-11-07 18:40:47 -0600

    Initial commit. Should be a working plugin. If not, it's sure close.

---
+++ lib/test_core_ext.rb
@@ -0,0 +1,20 @@
+require 'core_ext'
+
+module Test # :nodoc: all
+  module Unit
+    class TestCase
+
+      ##
+      # Time warp to the specified time for the duration of the passed block.
+      def pretend_now_is(time)
+        begin
+          Time.testing_offset = Time.now - time
+          yield
+        ensure
+          Time.testing_offset = 0
+        end
+      end
+
+    end
+  end
+end
