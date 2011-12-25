07a08bd160f49bc3e73fc33665a6846ff5f54902
Orban Botond <orbanbotond@gmail.com>
2011-12-25 19:10:48 +0200

    Rspec compatibility added

--- lib/time_warp.rb
+++ lib/time_warp.rb
@@ -34,3 +34,30 @@ module Test # :nodoc:
     end
   end
 end
+
+module RSpec
+  module Core
+    class ExampleGroup
+     # Time warp to the specified time for the duration of the passed block.
+      def reset_to_real_time
+        Time.testing_offset = 0
+      end
+      def pretend_now_is(*args)
+        Time.testing_offset = Time.now - time_from(*args)
+        if block_given?
+          begin
+            yield
+          ensure
+            reset_to_real_time
+          end
+        end
+      end
+    private
+      def time_from(*args)
+        return args[0] if 1 == args.size && args[0].is_a?(Time)
+        return args[0].to_time if 1 == args.size && args[0].respond_to?(:to_time)  # For example, if it's a Date.
+        Time.utc(*args)
+      end
+    end
+  end
+end
