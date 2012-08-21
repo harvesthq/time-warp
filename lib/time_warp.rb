c9b3bd80e808e82272c7db8beac43a05f59c9033
Ben MacLeod <ben.j.macleod@gmail.com>
2012-08-21 14:31:35 +1000

    Also, don't accidentally define MiniTest and Test

--- lib/time_warp.rb
+++ lib/time_warp.rb
@@ -2,42 +2,46 @@ require File.join File.dirname(__FILE__), 'core_ext'
 
 module TimeWarpAbility
 
-    def reset_to_real_time
-      Time.testing_offset = 0
-    end
+  def reset_to_real_time
+    Time.testing_offset = 0
+  end
 
-    def pretend_now_is(*args)
-      Time.testing_offset = Time.now - time_from(*args)
-      if block_given?
-        begin
-          yield
-        ensure
-          reset_to_real_time
-        end
+  def pretend_now_is(*args)
+    Time.testing_offset = Time.now - time_from(*args)
+    if block_given?
+      begin
+        yield
+      ensure
+        reset_to_real_time
       end
     end
+  end
 
   private
 
-    def time_from(*args)
-      return args[0] if 1 == args.size && args[0].is_a?(Time)
-      return args[0].to_time if 1 == args.size && args[0].respond_to?(:to_time)  # For example, if it's a Date.
-      Time.utc(*args)
-    end
+  def time_from(*args)
+    return args[0] if 1 == args.size && args[0].is_a?(Time)
+    return args[0].to_time if 1 == args.size && args[0].respond_to?(:to_time)  # For example, if it's a Date.
+    Time.utc(*args)
+  end
 end
 
-module Test # :nodoc:
-  module Unit # :nodoc:
-    class TestCase
-      include ::TimeWarpAbility
+if defined?(Test)
+  module Test # :nodoc:
+    module Unit # :nodoc:
+      class TestCase
+        include ::TimeWarpAbility
+      end
     end
   end
 end
 
-module MiniTest
-  class Unit
-    class TestCase
-      include ::TimeWarpAbility
+if defined?(MiniTest)
+  module MiniTest
+    class Unit
+      class TestCase
+        include ::TimeWarpAbility
+      end
     end
   end
 end
@@ -47,7 +51,7 @@ if defined?(RSpec)
     module Core
       class ExampleGroup
         include ::TimeWarpAbility
-       # Time warp to the specified time for the duration of the passed block.
+        # Time warp to the specified time for the duration of the passed block.
       end
     end
   end
